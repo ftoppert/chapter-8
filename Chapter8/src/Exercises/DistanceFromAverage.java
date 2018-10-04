528eae529ca22013c079c9a0e826ea9a5593763c
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-10-04 08:28:51 -0500

    DistanceFromAverage

--- Chapter8/src/Exercises/DistanceFromAverage.java
+++ Chapter8/src/Exercises/DistanceFromAverage.java
@@ -1,10 +1,18 @@
 package Exercises;
 
+import java.util.Scanner;
+
 public class DistanceFromAverage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+double[] arrayNums = {2,4,6,8,10,12,14,16,18,20};
+int exitChoice;
+Scanner input = new Scanner(System.in);
+for(int )
+	System.out.print("enter a number 99999 to exit any other number to continue>>> ");
+		exitChoice = input.nextInt();	
 	}
 
 }
